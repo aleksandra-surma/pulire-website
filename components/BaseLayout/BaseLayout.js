4d87368bdf00cd0267fbac2c46cc35859db27e71
sebastian-lucjan <sebastian.lucjan@gmail.com>
2022-01-06 11:40:35 +0100

    Add Loader spinner and fix performance warrning
    Fix performance warrning by add useEffect with clearing func with Router.events.off

--- components/BaseLayout/BaseLayout.js
+++ components/BaseLayout/BaseLayout.js
@@ -5,10 +5,27 @@ import PageWrapper from 'components/PageWrapper/PageWrapper';
 import { PageContext } from 'data/pageContext';
 import MobileNavigation from 'components/MobileNavigation/MobileNavigation';
 import useMobileNav from 'hooks/useMobileNav';
+import { useEffect, useState } from 'react';
+import { Router } from 'next/router';
+import PuffLoader from 'react-spinners/PuffLoader';
 
 export default function BaseLayout({ children, currentPageUrl = '/' }) {
-  const { isHamburger, isMobileMenuActive, toggleMenuActive } = useMobileNav();
+  const { isHamburger, isMobileMenuActive, isDesktop, toggleMenuActive } = useMobileNav();
+  const [isLoading, setIsLoading] = useState(false);
   const providedData = { currentPage: currentPageUrl };
+
+  useEffect(() => {
+    const startLoaded = () => setIsLoading(true);
+    const completeLoaded = () => setIsLoading(false);
+
+    Router.events.on('routeChangeStart', startLoaded);
+    Router.events.on('routeChangeComplete', completeLoaded);
+
+    return () => {
+      Router.events.off('routeChangeStart', startLoaded);
+      Router.events.off('routeChangeComplete', completeLoaded);
+    };
+  }, [isLoading]);
 
   return (
     <PageContext.Provider value={providedData}>
@@ -19,9 +36,16 @@ export default function BaseLayout({ children, currentPageUrl = '/' }) {
             toggleMenuActive={toggleMenuActive}
             isHamburger={isHamburger}
           />
-          <ViewWrapper>{children}</ViewWrapper>
-          {/* {!isMobileMenuActive ? <ViewWrapper>{children}</ViewWrapper> : null} */}
-          {isHamburger && isMobileMenuActive ? <MobileNavigation toggleMenuActive={toggleMenuActive} /> : null}
+          {isLoading && !isDesktop ? (
+            <div className="flex fix top-0 left-0 overflow-hidden justify-center items-center w-screen h-screen bg-white">
+              <PuffLoader size={120} />
+            </div>
+          ) : (
+            <ViewWrapper>{children}</ViewWrapper>
+          )}
+          {isHamburger && isMobileMenuActive ? (
+            <MobileNavigation setIsLoading={setIsLoading} toggleMenuActive={toggleMenuActive} />
+          ) : null}
         </div>
         {isHamburger && isMobileMenuActive ? null : <Footer />}
       </PageWrapper>
