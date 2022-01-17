c90c4d3a590a9c31351746cfcde423dacc3ec8da
sebastian-lucjan <sebastian.lucjan@gmail.com>
2022-01-17 15:31:22 +0100

    Fix 404 behaviour and scrollTop bottom / right vaules

--- components/ScrollTop/index.js
+++ components/ScrollTop/index.js
@@ -3,7 +3,7 @@ import { animateScroll } from 'react-scroll';
 
 const ScrollTop = () => {
   return (
-    <div className="fixed right-8 bottom-4">
+    <div className="fixed right-10 bottom-6">
       <button type="button" onClick={animateScroll.scrollToTop}>
         <Icon className="scrollToTopIcon" icon="ph:caret-up-bold" width={30} height={30} />
       </button>
--- pages/404.js
+++ pages/404.js
@@ -1,12 +1,14 @@
 import BaseLayout from 'components/BaseLayout';
-import pages from 'data/paths';
 import Image from 'next/image';
 import Title from 'components/Title';
 import napkinsImg from 'public/images/napkins-pulire.jpeg';
+import { useRouter } from 'next/router';
 
 const FourZeroFour = () => {
+  const router = useRouter();
+
   return (
-    <BaseLayout currentPageUrl={pages.home}>
+    <BaseLayout currentPageUrl={router.pathname}>
       <section className="flex flex-col lg:flex-row lg:items-center lg:justify-between text-justify w-full min-h-pageView tall:min-h-[70vh] tallMobile:min-h-[calc(100vh-100px)] font-redHat">
         <div className="flex pt-6 flex-col w-full h-full justify-center items-center lg:w-2/5 text-base xs:text-lg tablet:text-xl">
           <Title title="404" />
@@ -14,7 +16,7 @@ const FourZeroFour = () => {
           <p>Proszę wybierz inny adres.</p>
         </div>
         <div className="p-2 py-8 lg:p-8 lg:w-1/2 lg:py-16">
-          <Image src={napkinsImg} alt="Serwetki z logiem pulire" />
+          <Image priority src={napkinsImg} alt="Serwetki z logiem pulire" />
         </div>
       </section>
     </BaseLayout>
