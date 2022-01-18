a94ebf4d1adf867e72a9da0ef129e989fdb4fc43
sebastian-lucjan <sebastian.lucjan@gmail.com>
2022-01-18 14:47:16 +0100

    Sending form message dependet on new IS_PROD env variable

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
-module.exports = {
-  reactStrictMode: true,
-  env: {
+const { PHASE_PRODUCTION_BUILD } = require('next/constants');
+
+module.exports = (phase) => {
+  const reactStrictMod = true;
+
+  // const isDev = phase === PHASE_DEVELOPMENT_SERVER;
+  const isProd = phase === PHASE_PRODUCTION_BUILD;
+
+  const env = {
     SITE_KEY: process.env.SITE_KEY,
     SECRET_KEY: process.env.SECRET_KEY,
     MAIL_USER: process.env.MAIL_USER,
     MAIL_PASSWORD: process.env.MAIL_PASSWORD,
-  },
+    IS_PROD: isProd,
+  };
 };
