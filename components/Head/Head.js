5a440cac0cd3d6c5fcdd0c586910d49c16df79bc
sebastian-lucjan <sebastian.lucjan@gmail.com>
2022-01-19 14:23:11 +0100

    Google Analitics - fix bug

--- components/Head/Head.js
+++ components/Head/Head.js
@@ -21,6 +21,19 @@ const HeadContainer = () => {
         href="https://fonts.googleapis.com/css2?family=Barlow:wght@400;600&family=Red+Hat+Display:wght@400;600;700&display=swap"
         rel="stylesheet"
       />
+      <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_VERCEL_ID}`} />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.GA_VERCEL_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <title>Pulire - kompleksowe sprzątanie obiektów</title>
       <meta name="robots" content="noindex" />
       {/* <meta name="robots" content="all" /> */}
