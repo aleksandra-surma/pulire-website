26b4fa1c09664ae2ef9850dba11671693da15823
sebastian-lucjan <sebastian.lucjan@gmail.com>
2022-01-21 13:24:35 +0100

    Add open graph 'og:' metatags for all paths

--- components/HeadSection/index.js
+++ components/HeadSection/index.js
@@ -1,9 +1,13 @@
 import Head from 'next/head';
 
-const HeadSection = ({ title, description, index = true }) => {
+const HeadSection = ({ title, description, ogData, index = true }) => {
   return (
     <Head>
       <meta name="description" content={description} />
+      <meta property="og:title" content={ogData.title} />
+      <meta property="og:description" content={ogData.description} />
+      <meta property="og:url" content={ogData.url} />
+      <meta property="og:type" content="website" />
       <title>{title}</title>
       {index ? <meta name="robots" content="noindex" /> : <meta name="robots" content="noindex" />}
       {/*  //todo: change in production to: <meta name="robots" content="all" /> */}
