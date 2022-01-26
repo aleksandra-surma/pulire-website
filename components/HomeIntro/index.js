5203b4bc4dfc00018b1600b437c57836dc806e93
sebastian-lucjan <sebastian.lucjan@gmail.com>
2022-01-26 10:30:17 +0100

    Fix home sponge center Y place

--- components/HomeIntro/index.js
+++ components/HomeIntro/index.js
@@ -32,7 +32,7 @@ export default function HomeIntro() {
         <h2 className="pb-8 text-3xl text-left font-semibold">{title}</h2>
         <p className="text-left xxs:text-justify">{description}</p>
       </div>
-      <div className="min-h-[calc(100vh-80px)] relative flex justify-center w-[90vw] h-[calc(100vh-180px)] lg:mx-20 justify-center lg:w-2/5 lg:max-w-[700px]">
+      <div className="min-h-[calc(100vh-160px)] mb-[80px] relative flex justify-center w-[90vw] h-[calc(100vh-180px)] lg:mx-20 justify-center lg:w-2/5 lg:max-w-[700px]">
         <Image
           src={isDesktop ? home.homeUrlDesktop : home.homeUrl}
           priority
