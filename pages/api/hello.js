7db5ddf37f3bad904a152a92af6f335bbf6183d8
sebastian-lucjan <sebastian.lucjan@gmail.com>
2022-02-04 13:39:32 +0100

    Add sitemap.xml and delete unnecessary api asset: hello.js

--- pages/api/hello.js
+++
@@ -1,8 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default function helloAPI(req, res) {
-  if (req.method === 'GET') {
-    console.log('test GET next');
-  }
-  res.status(200).json({ name: 'John Doe' });
-}
---
+++ pages/sitemap.xml.js
@@ -0,0 +1,55 @@
+import fs from 'fs';
+
+const Sitemap = () => {};
+
+export const getServerSideProps = ({ res }) => {
+  const baseUrl = {
+    development: 'http://localhost:3000',
+    // production: 'https://pulire.co', //todo: final change!
+    production: 'https://pulire-predeploy.vercel.app',
+  }[process.env.NODE_ENV];
+
+  const staticPages = fs
+    .readdirSync('pages')
+    .filter((staticPage) => {
+      return !['_app.js', 'api', '_document.js', 'index.js', '_error.js', '404.js', 'sitemap.xml.js'].includes(
+        staticPage,
+      );
+    })
+    .map((staticPagePath) => {
+      return `${baseUrl}/${staticPagePath}`;
+    });
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      <url>
+        <loc>${baseUrl}</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>1.0</priority>
+      </url>
+      ${staticPages
+        .map((url) => {
+          return `
+            <url>
+              <loc>${url}</loc>
+              <lastmod>${new Date().toISOString()}</lastmod>
+              <changefreq>monthly</changefreq>
+              <priority>0.8</priority>
+            </url>
+          `;
+        })
+        .join('')}
+    </urlset>
+  `;
+
+  res.setHeader('Content-Type', 'text/xml');
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+};
+
+export default Sitemap;
