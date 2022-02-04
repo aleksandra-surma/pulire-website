e326c34a075b8a9ebdfdbdf78fb338c5bf22d4f8
sebastian-lucjan <sebastian.lucjan@gmail.com>
2022-02-04 12:40:43 +0100

    Update keywords and 404 head section

--- components/HeadMeta/index.js
+++ components/HeadMeta/index.js
@@ -14,7 +14,6 @@ const HeadMeta = () => {
       <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
       <link rel="manifest" href="/site.webmanifest" />
       <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" />
-      {/* <link rel="icon" href="/icons/favicon.ico" /> */}
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
       <link
@@ -24,7 +23,7 @@ const HeadMeta = () => {
       <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_VERCEL_ID}`} />
       <meta
         name="keywords"
-        content="pulire, sprzątanie lublin, sprzątanie hoteli Lublin, sprzątanie obiektów komercyjnych Lublin, firma sprzątająca Lublin, sprzątanie obiektów przemysłowych, sprzątanie obiektów handlowo-usługowych, sprzątanie biur, sprzątanie hoteli, sprzątanie budowlane, sprzątanie poremontowe lublin, sprzątanie pobudowlane"
+        content="pulire, sprzątanie lublin, sprzątanie hoteli Lublin, sprzątanie obiektów komercyjnych Lublin, firma sprzątająca Lublin, sprzątanie obiektów handlowo-usługowych, sprzątanie biur, sprzątanie poremontowe lublin"
       />
       <meta property="og:locale" content="pl_PL" />
       {/* <meta property="fb:app_id" content="ХХХХХХХХХХХХХХХ" /> todo: add app_id - https://developers.facebook.com/docs/sharing/webmasters#markup  */}
